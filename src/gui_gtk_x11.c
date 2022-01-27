db52d2b839ee409d8e27f9fbce3c232572d40423
lilydjwg <lilydjwg@gmail.com>
2022-01-27 14:04:50 +0800

    remove #if because we use gui_mch_get_display in more places

--- src/gui_gtk_x11.c
+++ src/gui_gtk_x11.c
@@ -6250,9 +6250,6 @@ gui_get_x11_windis(Window *win, Display **dis)
 }
 #endif
 
-#if defined(FEAT_CLIENTSERVER) \
-	|| (defined(FEAT_X11) && defined(FEAT_CLIPBOARD)) || defined(PROTO)
-
     Display *
 gui_mch_get_display(void)
 {
@@ -6262,7 +6259,6 @@ gui_mch_get_display(void)
     else
 	return NULL;
 }
-#endif
 
     void
 gui_mch_beep(void)
